8c2d2ec83ede409dafdb4de61d3aff0f7b56c6f4
xiaohaoxing <xiaohaoxing@outlook.com>
2023-03-30 20:26:22 +0800

    feat: JNI

--- src/main/java/edu/whu/hytra/core/edu_whu_hytra_core_NativeTool.cpp
+++ src/main/java/edu/whu/hytra/core/edu_whu_hytra_core_NativeTool.cpp
@@ -3,7 +3,7 @@
 
 JNIEXPORT jstring JNICALL Java_edu_whu_hytra_core_NativeTool_Java_1Hello_1sayHello(JNIEnv *env, jobject)
 {
-    return env->NewStringUTF(env, "hello");
+    return env->NewStringUTF("hello");
 }
 
 
